fac747de05b05fbb80b5be2569525318399a308e
Luca Matei <dev@lucamatei.eu>
2022-12-07 01:49:22 +0200

    Fixed bugs

--- src/install.py
+++ src/install.py
@@ -271,3 +271,309 @@ def log(*args, **kwargs):
     logs._log(call_info, *args, **kwargs)
 
 
+class DbUtils:
+    query = """sudo -u hal psql -tAc \"{}\""""
+
+    def create_db(self, host=None):
+        pass
+
+    def config_pg(self, host=None):
+        pass
+
+    def create_pgrole(self, lmid, host=None):
+        log(f"Creating {lmid} role ...", console=True)
+        password = utils.new_pass(64)
+
+        if lmid == "hal":
+            query = self.query.replace("hal", "postgres")
+        else:
+            query = self.query
+
+        role_query = query.format(f"create role {lmid} with login {'createdb createrole ' if lmid == 'hal' else ''}password '{password}';")
+        output = cmd(role_query, catch=True)
+        if "already exists" in output:
+            log(f"{lmid} role already exists!", console=True)
+            yes = utils.yes_no("Purge it?")
+
+            if yes: cmd(query.format(f"drop database {lmid} if exists; drop role {lmid};"))
+            else: return
+
+            cmd(role_query)
+
+        if lmid == "hal":
+            return
+
+        elif lmid.startswith("lm"):
+            details_path = utils.projects_dir + lmid + "/src/app/db/details.ast"
+            details = utils.read(details_path)
+            details['pass'] = password
+            utils.write(details_path, settings)
+            return password
+
+        else:
+            utils.write(utils.tmp_dir + "db_pass.tmp", password)
+            log(f"Password stored in {utils.tmp_dir}db_pass.tmp!", console=True)
+
+    def create_pgdb(self, lmid, host=None):
+        log(f"Creating {lmid} database ...", console=True)
+        db_query = self.query.format(f"create database {lmid} owner {lmid} encoding 'utf-8';")
+        output = cmd(db_query, catch=True)
+        if "already exists" in output:
+            log(f"{lmid} database already exists!", console=True)
+            yes = utils.yes_no("Purge it?")
+
+            if yes: cmd(self.query.format(f"drop database {lmid};"))
+            else: return
+
+            cmd(db_query, catch=True)
+
+utils.dbs = DbUtils()
+
+
+class GPG:
+    def create_gpgkey(self, email):
+        log(f"Generating GPG key for '{email}'. This may take a while ...", console=True)
+
+        key_config = utils.format_tpl("gpg-key.tpl", {
+            "user": email.split('@')[0],
+            "email": email
+            })
+        utils.write(utils.tmp_dir + "gpg", key_config)
+
+        cmd(f"gpg2 --batch --gen-key {utils.tmp_dir}gpg")
+
+        return self.get_privkey_id(email)
+
+    def get_privkey_id(self, email):
+        privkey_id = cmd(f"gpg2 --list-secret-keys --keyid-format LONG {email}", catch=True)
+        if not "No secret key" in privkey_id:
+            return re.findall(r'\bsec   rsa4096/\w+', privkey_id)[0].split('/')[1]
+        else:
+            log(f"Couldn't find GPG key for '{email}'!", level=4, console=True)
+            yes = utils.yes_no("Create one?")
+            if yes:
+                return self.create_gpgkey(email)
+            return 0
+
+    def delete_gpgkey(self, host):
+        cmd(f"gpg2 --batch --delete-secret-keys {email}")
+        cmd(f"gpg2 --batch --delete-keys {email}")
+
+gpg = GPG()
+
+
+class Install:
+    def start(self):
+        self.src_dir = utils.get_src_dir()
+        self.opts = utils.read(self.src_dir + "install.ast")
+        self.lmid = self.opts['lmid']
+
+        self.create_sudo()
+        print()
+        self.install_deps()
+        print()
+        self.create_user()
+        print()
+        self.add_to_group()
+        print()
+        self.create_dir_tree()
+        print()
+        self.create_env()
+        print()
+        self.create_cmd()
+        print()
+
+        if self.opts['is_main']:
+            self.place_hal()
+            print()
+
+        self.config_git()
+
+        cmd(utils.projects_dir + self.lmid + "/make")
+
+        if self.opts['has_db']:
+            self.config_pg()
+            print()
+
+        if self.opts['has_web']:
+            self.config_web()
+            print()
+
+    def abort(self, msg):
+        print(f"{msg} Aborting ...")
+        sys.exit()
+
+    def create_sudo(self):
+        if not "ok installed" in cmd(f"dpkg -s sudo | grep Status", catch=True):
+            print("Installing sudo ...")
+            cmd("apt install sudo")
+
+        print("Giving sudo permissions to Hal ...")
+        cmd_path = "/usr/bin/"
+        cmds = "ALL"
+        #cmds = "apt",
+        #cmds = ', '.join(cmd_path + c for c in cmds)
+        cmd(f"echo 'hal ALL=(ALL) NOPASSWD: {cmds}' > /etc/sudoers.d/hal")
+
+    def install_deps(self):
+        print("Installing dependencies ...")
+        packages = "build-essential", "python3", "python3-dev", "python3-venv", "python3-pip", "gnupg2",
+
+        if self.opts['has_web']:
+            packages += "openssl", "nginx", "supervisor",
+
+        if self.opts['has_web'] or self.opts['has_db']:
+            packages += "postgresql", "libpq-dev",
+
+        if self.opts['has_ssh_server']:
+            packages += "openssh-server",
+
+        if self.opts['has_ssh_client']:
+            packages += "openssh-client",
+
+        if self.opts['has_vms']:
+            # packages +=
+            pass
+
+        if self.opts['has_dhcp']:
+            packages += "isc-dhcp-server"
+
+        if self.opts['has_dns']:
+            packages += "bind9"
+
+        if self.opts['has_firewall']:
+            packages += "nftables",
+
+        for package in packages:
+            if not "ok installed" in cmd(f"dpkg -s {package} | grep Status", catch=True):
+                cmd(f"apt install {package} -y", catch=True)
+
+    def create_user(self):
+        # https://manpages.debian.org/jessie/adduser/adduser.8.en.html
+        if not cmd("getent passwd hal", catch=True):
+            print("Creating 'hal' user and group ...")
+            cmd("adduser --system --group --no-create-home --disabled-login --gecos '' hal", catch=True)
+            cmd(f"echo hal:{utils.new_pass(64)} | chpasswd")
+        else:
+            print("User 'hal' already exists!")
+            yes = utils.yes_no("Use it?")
+            if not yes:
+                self.abort("Can't use user 'hal'!")
+
+    def add_to_group(self):
+        users = ', '.join([x.split(':')[-1] for x in cmd("sudo -u hal getent group hal", catch=True).split('\n')])
+        if users:
+            print(f"Users already in Hal's group: {users}.")
+            yes = utils.yes_no("Add another one?")
+            if not yes:
+                return
+
+        user = ""
+        output = "does not exist"
+
+        while "does not exist" in output:
+            user = input("Enter an user to access Hal's files: ")
+            output = cmd(f"sudo -u hal usermod -a -G hal {user}", catch=True)
+
+        print(f"{user} has to logout and login again to have access to Hal's files!")
+
+    def create_dir_tree(self):
+        print("Creating Hal's directory tree ...")
+
+        dir_tree = utils.logs_dir, utils.mnt_dir, utils.projects_dir, utils.res_dir, utils.ssh_dir, utils.ssl_dir, utils.tmp_dir, utils.vms_dir,
+
+        if not os.path.isdir(utils.hal_dir):
+            cmd(f"mkdir {utils.hal_dir}")
+            cmd(f"chown hal:hal {utils.hal_dir}")
+            cmd(f"chmod g+rwx {utils.hal_dir}")
+
+        for node in dir_tree:
+            # It's a directory
+            if node.endswith('/') and not os.path.isdir(node):
+                cmd(f"sudo -u hal mkdir {node}")
+
+            # It's a file
+            elif not os.path.isfile(node):
+                cmd(f"sudo -u hal touch {node}")
+
+    def create_env(self):
+        if os.path.isdir(f"{utils.projects_dir}venv/"):
+            print("There's already a Virtual Env!")
+            yes = utils.yes_no("Purge it?")
+
+            if yes: cmd(f"sudo -u hal rm -r {utils.projects_dir}venv/")
+            else: return
+
+        print("Creating Virtual Env ...")
+        cmd(f"sudo -u hal python3 -m venv {utils.projects_dir}venv")
+
+        packages = "wheel",
+        if self.opts['has_web']:
+            packages += "uwsgi", "libsass", "pyyaml",
+
+        if self.opts['has_web'] or self.opts['has_db']:
+            packages += "psycopg2",
+
+        for package in packages:
+            cmd(f"sudo -u hal {utils.projects_dir}venv/bin/pip install {package}")
+
+    def create_cmd(self):
+        print("Creating 'hal' command ...")
+        hal_bash = utils.format_tpl("hal-bash.tpl", {"lmid": self.lmid})
+        utils.write("/usr/local/bin/hal", hal_bash)
+        cmd("chmod +x /usr/local/bin/hal")
+
+    def place_hal(self):
+        # To do: download lm1-versions, lm2, lm2-versions
+        if os.path.isdir(utils.projects_dir + self.lmid):
+            print(f"Hal already is in the right place!")
+            yes = utils.yes_no("Purge it?")
+            if yes:
+                cmd(f"sudo -u hal rm -r {utils.projects_dir + self.lmid}")
+            else:
+                return
+
+        print("Placing Hal in the right place ...")
+        cmd(f"sudo -u hal git clone https://gitlab.com/lucamatei/{self.lmid}.git {utils.projects_dir + self.lmid}/")
+
+    def config_git(self):
+        log("Configuring Git ...", console=True)
+        config = utils.format_tpl("gitconfig.tpl", {
+            "user": utils.hostname,
+            "email": f"{utils.hostname}@{self.opts['gitlab']}",
+            "gpg_key": gpg.get_privkey_id(utils.hostname)
+            })
+        utils.write(f"/home/hal/.gitconfig", config)
+
+        if not self.opts['gitlab_token']:
+            print("Please enter Gitlab REST API token.")
+            token = getpass.getpass("Token: ")
+            settings = utils.read(self.src_dir + "app/settings.ast")
+            settings['gitlab_token'] = token
+            utils.write(self.src_dir + "app/settings.ast", settings)
+
+    def config_pg(self):
+        # https://www.postgresql.org/docs/current/sql-createrole.html
+        print("Configuring PostgreSQL ...")
+        utils.dbs.create_pgrole("hal")
+
+        if self.opts['is_main']:
+            utils.dbs.create_pgrole(self.lmid)
+            utils.dbs.create_pgdb(self.lmid)
+
+help = """
+    Help
+    """
+
+args = sys.argv[1:] + ['']
+if args[0] in ("-h", "help"):
+    print(help)
+    sys.exit()
+
+if getpass.getuser() == "root":
+    install = Install()
+    install.start()
+else:
+    print("You have to run this script with root priviledges!")
+
+
